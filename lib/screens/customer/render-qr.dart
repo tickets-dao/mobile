247d396ef5d4a1f8d3eea01f857a7ff21eeb0ea4
BruceAndyLee <andruha34@mail.ru>
2023-05-09 16:55:03 +0300

    removed build-breaking imports

--- lib/screens/customer/render-qr.dart
+++ lib/screens/customer/render-qr.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:flutter/services.dart';
 import 'dart:async';
 import 'dart:typed_data';
-import 'dart:ui';
+// import 'dart:ui';
 import 'dart:io';
-import 'package:flutter/rendering.dart';
-import 'package:path_provider/path_provider.dart';
 
 class GenerateScreen extends StatefulWidget {
   @override
